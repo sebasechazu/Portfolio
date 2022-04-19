b1acbb7c55e251de51d70c1e2cb30100c4b66218
cande342 <gcandela894@gmail.com>
2022-04-19 12:07:46 -0300

    arreglar el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavegadorComponent } from './navegador/navegador.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavegadorComponent
+    NavegadorComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule
